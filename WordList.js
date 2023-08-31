619fccbb8781a0403fdf8af276600064dceb8c3e
omarbarbeir <138704845+omarbarbeir@users.noreply.github.com>
2023-09-01 00:39:24 +0300

    Add files via upload

---
+++ WordList.js
@@ -0,0 +1,24 @@
+let list =[
+
+    {
+        word: "guitar",
+        hint: "A musical instrument with strings"
+    },
+    {
+        word: "oxygen",
+        hint: "A colorless, odorless gas essential for life"
+    },
+    {
+        word: "mountain",
+        hint: "A larg natural elevation of the Earth surface"
+    },
+    {
+        word: "painting",
+        hint: "An art from using colors on a surface to create images of expression"
+    },
+    {
+        word: "astronomy",
+        hint: "The scientific study of xelestial objects and phenomena"
+    }
+
+]
